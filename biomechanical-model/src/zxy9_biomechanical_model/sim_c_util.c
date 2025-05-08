4aa2f0aca6516c05e2d18b12a2e41e86c3f57daf
Sam Liepke <samliepke@gmail.com>
2025-05-08 10:30:51 -0400

    removed duplicate readme, fixed some indentation

--- biomechanical-model/src/zxy9_biomechanical_model/sim_c_util.c
+++ biomechanical-model/src/zxy9_biomechanical_model/sim_c_util.c
@@ -18,11 +18,10 @@
 
 // Module def
 static struct PyModuleDef module_def = {
-    PyModuleDef_HEAD_INIT,
-    "sim_c_util",   /* name of module */
-    NULL, /* module documentation, may be NULL */
-    -1,       /* size of per-interpreter state of the module,
-                 or -1 if the module keeps state in global variables. */
+	PyModuleDef_HEAD_INIT,
+	"sim_c_util",   /* name of module */
+	NULL, /* module documentation, may be NULL */
+	-1
 };
 
 // Module init
@@ -30,24 +29,24 @@ PyMODINIT_FUNC
 PyInit_sim_c_util(void)
 {
 	// create module
-    PyObject *m = PyModule_Create(&module_def);
+	PyObject *m = PyModule_Create(&module_def);
 	if (m == NULL)
 		return NULL;
 	
 	// initialize numpy API
-    import_array();
+	import_array();
 	
 	// add Sim_Entry class to module
-    if (PyType_Ready(&Sim_Entry_Type) < 0) {
-        Py_DECREF(m);
-        return NULL;
+	if (PyType_Ready(&Sim_Entry_Type) < 0) {
+		Py_DECREF(m);
+		return NULL;
 	}
-    if (PyModule_AddObjectRef(m, "Sim_Entry", (PyObject *) &Sim_Entry_Type) < 0) {
-        Py_DECREF(m);
-        return NULL;
+	if (PyModule_AddObjectRef(m, "Sim_Entry", (PyObject *) &Sim_Entry_Type) < 0) {
+		Py_DECREF(m);
+		return NULL;
 
-    }
+	}
 	
 	// return
-    return m;
+	return m;
 }
